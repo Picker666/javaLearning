6a0c16cac6794cc0e35c5310bec46cdb094edad3
picker.ren <picker.ren@ap.jll.com>
2022-09-26 14:35:33 +0800

    scnner learning

--- basic-app/src/operatorDemo.java
+++ basic-app/src/operatorDemo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class operatorDemo {
+import java.util.Scanner;
+
+public class operatorDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请您输入年龄：");
+        int age = scanner.nextInt();
+        System.out.println("请您输入年龄：" + age);
+
+        System.out.println("请输入您的名字:");
+        String name = scanner.next();
+        System.out.println("请输入您的名字:" + name);
+    }
 }
