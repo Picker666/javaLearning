260b78a105a1c2d96db6703b7789e033fe15ce04
picker.ren <picker.ren@ap.jll.com>
2022-09-26 17:11:52 +0800

    array create

--- flow-control-app/array/src/create/ArrayDemo1.java
+++ flow-control-app/array/src/create/ArrayDemo1.java
@@ -2,7 +2,7 @@ package create;
 
 public class ArrayDemo1 {
     public static void main(String[] args) {
-//        1、定义
+//        1、定义(静态初始化数组)
         double[] scores = new double[]{99, 88.8, 33.3,2312.23};
         int[] ages = new int[]{1,2,3,4,5};
         String[] name = new String[]{"fadf", "dfasd"};
@@ -15,5 +15,26 @@ public class ArrayDemo1 {
 //        2、数组的访问
         System.out.println(scores[0]);
         System.out.println(scores.length);
+
+//        3、动态初始化数组
+//        定义数组的时候值确定初始化数组的长度，之后再存入具体的数据
+
+        double[] scoress = new double[3];
+
+        scoress[0] = 99.9;
+        System.out.println(scoress[1]);
+
+        String[] nms = new String[3];
+        nms[0] = "马尔扎哈";
+        System.out.println(nms[0] + nms[2]);
+
+//        4、动态初始化 元素的默认值
+//        （1）、整型数组的元素默认值： 0；
+//        （2）、字符类型数组的元素默认值是： 0
+        char[] ch = new  char[3];
+        System.out.println((int)ch[0]);
+//        （3）、浮点型的元素默认值： 0.0
+//        （4）、布尔类型的数组默认值是： false
+//        （5）、String类型数组的默认值是： null
     }
 }
