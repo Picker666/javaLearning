5b0d07d1a97258461725ce0bd7f7ab3f766a7b9f
picker.ren <picker.ren@ap.jll.com>
2022-09-27 17:47:49 +0800

    verification code

--- a05-practice-app/src/verficationCode/Index.java
+++ a05-practice-app/src/verficationCode/Index.java
@@ -1,7 +1,54 @@
 package verficationCode;
 
+import java.util.Random;
+
 public class Index {
     public static void main(String[] args) {
+        String verificationCode = generateCodes(6);
+        System.out.println(verificationCode);
+    }
 
+    public static int randomValue (int range) {
+        int value = randomValue(range, 0);
+        return value;
+    }
+
+    public static int randomValue (int range, int offset) {
+        Random random = new Random();
+        int value = random.nextInt(range) + offset;
+
+        return value;
+    }
+
+    public static String getCode () {
+        int type = randomValue(3);
+
+        String code = "";
+
+        switch (type) {
+            case 0:
+                int value = randomValue(10);
+                code = String.valueOf(value);
+                break;
+            case 1:
+                int value1 = randomValue(26, 65);
+                code = String.valueOf((char)value1);
+                break;
+            case 2:
+                int value2 = randomValue(26, 97);
+                code = String.valueOf((char)value2);
+                break;
+        }
+
+        return code;
+    }
+
+    public static String generateCodes (int number) {
+        String code = "";
+        for (int i = 0; i < number; i++) {
+            code += getCode();
+        }
+
+        return code;
     }
 }
