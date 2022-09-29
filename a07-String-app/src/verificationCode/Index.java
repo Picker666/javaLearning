f85ab812da90950828e7715e3883accfae17bf60
picker.ren <picker.ren@ap.jll.com>
2022-09-29 18:07:08 +0800

    login

--- a07-String-app/src/verificationCode/Index.java
+++ a07-String-app/src/verificationCode/Index.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class Index {
     public static void main(String[] args) {
         verificationCode();
-
-        logIn();
     }
 
     public static void verificationCode () {
@@ -23,39 +21,5 @@ public class Index {
         }
         System.out.println(code);
     }
-
-    public static void logIn () {
-        Scanner scanner = new Scanner(System.in);
-
-        String name = scanKeyIn(scanner, "请输入名字：");
-        String password = scanKeyIn(scanner, "请输入密码：");
-        verify(scanner, name, password);
-    }
-
-    public static String scanKeyIn (Scanner scanner, String Remind) {
-        System.out.print(Remind);
-        String keyIn = scanner.next();
-        return keyIn;
-    }
-
-    public static void verify (Scanner scanner, String name, String password) {
-        int times = 3;
-        String nm = scanKeyIn(scanner, "请输入名字：");
-        if (!nm.equals(name)) {
-            System.out.println("用户名不存在");
-            return;
-        }
-        do {
-            String psw = scanKeyIn(scanner, "请输入密码：");
-
-            if (password.equals(psw)) {
-                System.out.println("登陆成功！");
-                return;
-            }
-        } while (times-- > 1);
-
-        System.out.println("登陆失败！");
-    }
-
 
 }
