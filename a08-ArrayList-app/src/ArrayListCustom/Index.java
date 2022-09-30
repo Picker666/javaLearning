fbb76136113c208b3a3a147cb565db58d29eb6f1
picker.ren <picker.ren@ap.jll.com>
2022-09-30 12:10:24 +0800

    movies refactor

--- a08-ArrayList-app/src/ArrayListCustom/Index.java
+++ a08-ArrayList-app/src/ArrayListCustom/Index.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Index {
     public static void main(String[] args) {
 
+        ArrayList<Movie> movies = moviesCreate();
+        moviesShow(movies);
+
+    }
+
+    public static ArrayList<Movie> moviesCreate () {
         Movie m1 = new Movie("《肖申克的救赎》", 9.8, "罗宾斯");
         Movie m2 = new Movie("《这个杀手不太冷静》", 9.0, "魏翔");
         Movie m3 = new Movie("《红海行动》", 9.7, "吴京");
@@ -15,8 +21,10 @@ public class Index {
         movies.add(m2);
         movies.add(m3);
 
-        System.out.println(movies);
+        return movies;
+    }
 
+    public static void moviesShow (ArrayList<Movie> movies) {
         int size = movies.size();
         for (int i = 0; i < size; i++) {
             Movie movie = movies.get(i);
