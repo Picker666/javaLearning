a08cec5dcac1ca3b3d8e3a1df8ab9b5d6a4a8721
picker.ren <picker.ren@ap.jll.com>
2022-10-13 17:29:10 +0800

    interface enhance

---
+++ a15-interface-app/src/interfaceEnhance/Index.java
@@ -0,0 +1,10 @@
+package interfaceEnhance;
+
+public class Index {
+    public static void main(String[] args) {
+        PingPangMan pingPangMan = new PingPangMan();
+        pingPangMan.run();
+
+        SportMan.inAdd();
+    }
+}
---
+++ a15-interface-app/src/interfaceEnhance/PingPangMan.java
@@ -0,0 +1,5 @@
+package interfaceEnhance;
+
+public class PingPangMan implements SportMan {
+
+}
